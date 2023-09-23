8c2d1b30bc7beab6ee0a759abdf013810069b2b6
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2023-09-23 10:12:37 +0100

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero";
+
+function Header() {
+  return (
+    <div>
+      <Hero />
+      <div>Navbar</div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,5 @@
+function Hero() {
+  return <div>Heroi</div>;
+}
+
+export default Hero;
